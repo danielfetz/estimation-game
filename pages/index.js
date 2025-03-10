357dd427cdf37917877701e8ac18ce30ccb08228
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-10 22:18:52 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+import GameSelector from '../components/GameSelector';
 import EstimationGame from '../components/EstimationGame';
 
 export default function Home() {
+  const [selectedGame, setSelectedGame] = useState(null);
+  
+  const handleSelectGame = (categoryId, gameId) => {
+    setSelectedGame({ categoryId, gameId });
+  };
+  
+  const handleBackToSelection = () => {
+    setSelectedGame(null);
+  };
+  
   return (
-    <div className="container mx-auto">
-      <EstimationGame />
+    <div className="container mx-auto py-8 px-4">
+      {selectedGame ? (
+        <EstimationGame 
+          categoryId={selectedGame.categoryId}
+          gameId={selectedGame.gameId}
+          onBackToSelection={handleBackToSelection}
+        />
+      ) : (
+        <GameSelector onSelectGame={handleSelectGame} />
+      )}
     </div>
   );
 }
